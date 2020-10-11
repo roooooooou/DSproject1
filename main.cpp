191270dd4088be03747cb46c60735ae5d858035a
roooooooou <hsuan021214@gmail.com>
2020-10-12 03:11:19 +0800

    10/12 03:11

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 class Block {
 public:
     string type;
+    int Pos_x, Pos_y;
     int data[block_h][block_w] = {0};
     int blocktype[20][block_h][block_w] = {
         {{0, 0, 0, 0},{0, 0, 0, 0},{1, 1, 1, 0},{0, 1, 0, 0}},
@@ -33,8 +34,9 @@ public:
         {{0, 0, 0, 0},{0, 0, 0, 0},{0, 0, 0, 0},{0, 0, 0, 0}}};
     string t[19] = { "T1","T2","T3","T4","L1","L2","L3","L4","J1","J2","J3","J4","S1","S2","Z1","Z2","I1","I2","O" };
     Block(){}
-    Block(string name) 
+    Block(string name, int y, int x) 
     {
+        Pos_x = x; Pos_y = y;
         for (int i = 0; i < 19; i++)
             if (name == t[i])
                 getdata(i);
@@ -48,9 +50,21 @@ public:
     }
 };
 
-class map {
+class Map {
 public:
     int MapWidth, MapHeight;
+    int** map;
+    Map(){}
+    Map(int H, int W) :MapWidth(W), MapHeight(H)
+    {
+        map = new int* [H];
+        for (int i = 0; i < H; i++)
+        {
+            map[i] = new int[W];
+            for (int j = 0; j < W; j++)
+                map[i][j] = 0;
+        }
+    }
 };
 
 int main()
@@ -60,8 +74,11 @@ int main()
     cin >> filename;
     fstream file;
     string testcase;
+
     file.open(filename.c_str(), ios::in);
     file >> w >> h;
+    Map Tetris(h+1, w+1);
+
     while (file >> testcase)
     {
         if (testcase == "End")
@@ -71,9 +88,9 @@ int main()
         }  
         else
         {
-            int startPos, Move;
-            file >> startPos >> Move;
-            cout << testcase << endl;
+            int StartPos, Move;
+            file >> StartPos >> Move;
+            Block newblock(testcase, 0, StartPos);
         }
     }
 
